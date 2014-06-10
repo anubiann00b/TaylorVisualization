923c701d3643e6cd31f6610921e4b9c81214e03f
Shreyas <skraman1999@gmail.com>
2014-06-09 22:10:03 -0700

    Postfix to infix! Achievement get!

--- src/taylor/Calculator.java
+++ src/taylor/Calculator.java
@@ -1,22 +1,27 @@
 package taylor;
 
 import java.awt.Graphics;
+import taylor.math.Function;
 
 public class Calculator {
     
     private String equation;
+    
+    private Function f;
     
     public boolean axis = true;
     public boolean grid = false;
     public boolean coords = true;
     
     public Calculator() {
-        equation = "";
+        equation = null;
     }
     
     public void setEquation(String newEquation) { equation = newEquation; }
 
     public void render(Graphics g) {
+        if (equation != null)
+            f = new Function(equation);
         g.drawRect(0,0,50,50);
     }
 }
--- src/taylor/math/Function.java
+++ src/taylor/math/Function.java
@@ -1,11 +1,53 @@
 package taylor.math;
 
+import java.util.Stack;
+
 public class Function {
     
+    public static String operators = "*/+-(";
     public Expression exp;
     
-    public Function(String s) {
+    public Function(String str) {
+        /*
+        StringBuilder sb = new StringBuilder();
+        String[] split = s.split(" ");
+        for (String str : split)
+            sb.append(str);
+        String eq = sb.toString();
+
+        char[] chars = eq.toCharArray();
+        */
         
+        Expression e1;
+        Expression e2;
+        Stack<String> stack = new Stack<String>();
+        
+        String newStr = "";
+        
+        String[] tokens = str.split(" ");
+        
+        System.out.println(str);
+        
+        for (String s : tokens) {
+            if (s.equals("(")) {
+                stack.push(s);
+            } else if (operators.contains(s)) {
+                    while (!stack.empty() && (operators.indexOf(stack.peek()) <= operators.indexOf(s)))
+                        newStr += stack.pop() + " ";
+                stack.push(s);
+            } else if (s.equals(")")) {
+                while (!stack.peek().equals("("))
+                    newStr += stack.pop() + " ";
+                stack.pop();
+            } else {
+                newStr += s + " ";
+            }
+        }
+        
+        while (!stack.empty())
+            newStr += stack.pop() + " ";
+        
+        System.out.println(newStr);
     }
     
     public int getY(int x) {
