59c69aa1892a30df321b117c73debbbc87d3adb1
Shreyas <skraman1999@gmail.com>
2014-06-10 17:35:33 -0700

    Can graph lines. Yay.

--- src/taylor/Calculator.java
+++ src/taylor/Calculator.java
@@ -1,5 +1,6 @@
 package taylor;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import taylor.math.Function;
 
@@ -22,6 +23,16 @@ public class Calculator {
     public void render(Graphics g, int width, int height) {
         if (equation != null)
             f = new Function(equation);
+        if (f != null) {
+            g.setColor(Color.red);
+            int oldY = f.getY(0);
+            for (int i=width/100;i<width;i+=width/100) {
+                int y = f.getY(i);
+                g.drawLine(i-width/100,oldY,i,y);
+                oldY = y;
+            }
+        }
+        g.setColor(Color.black);
         g.drawRect(0,0,width-1,height-1);
     }
 }
